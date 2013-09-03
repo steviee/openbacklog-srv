9baad172dc31f961d6abd98ad9cb4ab4f130db44
Stephan <stephan@mail.deltacity.org>
2013-09-03 14:49:55 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tpl::Application.config.secret_key_base = '4d38a06b91706f3e1a08f15472fed8bce7dcf549e4b1bea0bc0d1233562d36226220dbcb270d0ec03bbe41fb2af245038d002b5fc8803e34b14aa846defc246f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tpl::Application.config.session_store :cookie_store, key: '_tpl_session'
